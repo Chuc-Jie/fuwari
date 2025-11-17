97961d47bc306baad1e84fbf7fb16ad337832db5
Chuc-Jie <www_xkj@163.com>
2025-11-17 17:14:39 +0800

    modified:   astro.config.mjs 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,33 +8,33 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "友野 贺今林",
+	subtitle: "认知 运气 本金 缺一不可",
+	lang: "zh_CN", // 语言代码，例如：'en'、'zh_CN'、'ja' 等
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		hue: 250, // 主题颜色的默认色调，范围从 0 到 360。例如：红色：0，蓝绿色：200，青色：250，粉色：345
+		fixed: false, // 对访客隐藏主题颜色选择器
 	},
 	banner: {
 		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		src: "assets/images/demo-banner.png", // 相对于 /src 目录的路径。如果以 '/' 开头，则相对于 /public 目录
+		position: "center", // 等同于 object-position，仅支持 'top'、'center'、'bottom'。默认为 'center'
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: false, // 是否显示横幅图片的署名文本
+			text: "", // 要显示的署名文本
+			url: "", // （可选）指向原始作品或艺术家页面的 URL 链接
 		},
 	},
 	toc: {
-		enable: true, // Display the table of contents on the right side of the post
-		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+		enable: true, // 在文章右侧显示目录
+		depth: 2, // 目录中显示的最大标题深度，范围从 1 到 3
 	},
 	favicon: [
-		// Leave this array empty to use the default favicon
+		// 将此数组留空以使用默认网站图标
 		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		//   src: '/favicon/icon.png',    // 网站图标的路径，相对于 /public 目录
+		//   theme: 'light',              // （可选）'light' 或 'dark'，仅在为浅色和深色模式设置了不同网站图标时使用
+		//   sizes: '32x32',              // （可选）网站图标的尺寸，仅在拥有不同尺寸的网站图标时设置
 		// }
 	],
 };
@@ -46,33 +46,26 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
+			url: "https://github.com/Chuc-Jie/fuwari", // 内部链接不应包含基础路径，因为它会自动添加
+			external: true, // 显示外部链接图标，并在新标签页中打开
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/demo-avatar.png", // 相对于 /src 目录的路径。如果以 '/' 开头，则相对于 /public 目录
+	name: "友野",
+	bio: "认知 运气 本金 缺一不可",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			name: "NetEaseMusic",
+			icon: "tabler:brand-netease-music",
+			url: "https://music.163.com/#/user/home?id=1997803975",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/Chuc-Jie",
 		},
 	],
 };
@@ -84,7 +77,7 @@ export const licenseConfig: LicenseConfig = {
 };
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
-	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
-	// Please select a dark theme, as this blog theme currently only supports dark background color
+	// 注意：某些样式（例如背景颜色）正在被覆盖，请参阅 astro.config.mjs 文件。
+	// 请选择一个深色主题，因为此博客主题目前仅支持深色背景颜色
 	theme: "github-dark",
 };
